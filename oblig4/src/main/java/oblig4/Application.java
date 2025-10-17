d121b0e703a596f60527b8a7b1d50246f810e77d
h187455 <187455@stud.hvl.no>
2025-10-17 17:52:43 +0200

    yup

--- oblig4/src/main/java/oblig4/Application.java
+++ oblig4/src/main/java/oblig4/Application.java
@@ -1,7 +1,11 @@
 package oblig4;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Application.class, args);
     }
 }
