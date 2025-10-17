c0369f28f86cbbf039493afc31118daf2bb04a42
h187455 <187455@stud.hvl.no>
2025-10-17 18:59:59 +0200

    ny pom

---
+++ oblig4/src/main/java/oblig4/Person.java
@@ -0,0 +1,14 @@
+package oblig4;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class Person {
+    
+}
