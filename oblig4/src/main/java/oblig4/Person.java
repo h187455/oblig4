88f143ed8e4d7fa9810486d1f19ba4d0ec46f7f8
h187455 <187455@stud.hvl.no>
2025-10-17 19:32:46 +0200

    g

--- oblig4/src/main/java/oblig4/Person.java
+++ oblig4/src/main/java/oblig4/Person.java
@@ -10,5 +10,14 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Person {
+    @Size(min=2, max=20, message="Fornavn må inneholde fra 2 til 20")
+    @NotNull(message = "Fornavn er obligatorisk")
+    private String fornavn;
+
+    @Size(min = 2, max = 20, message = "Etternavn må inneholde fra 2 til 20")
+    @NotNull(message = "Etternavn er obligatorisk")
+    private String etternavn;
+
     
+
 }
